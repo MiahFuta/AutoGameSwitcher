b708496568f34bf6abbfdd7a512020ee02816e33
MiahFuta <sowhoyou@gmail.com>
2023-01-22 18:38:30 -0500

    minor style

--- js/twitch/switchgames.js
+++ js/twitch/switchgames.js
@@ -5,6 +5,7 @@ function copyToClipboard() {
     var text = readOnlyField.value;
     navigator.clipboard.writeText(text).then(
         function() {
+            alert("URL Copied! Use CTRL+V to Paste!\n\n" + text);
             console.log("Copying to clipboard was successful!");
         },
         function(err) {
@@ -36,15 +37,19 @@ class SwitchGames {
                     console.log('Current Game ID: ' + game_id);
                     console.log('Game Name: ' + game_name);
 
-                    $('body').append('<div>Current Game Name: ' + game_name + '</div>');
-                    $('body').append('<div>Current Game ID: ' + game_id + '</div><br>');
+                    $('body').append('<div><strong>Current Game Name</strong><br><br> ' + game_name + '</div><br><br>');
 
                     let browser_url =  location.protocol + '//' + location.host + location.pathname + '?game=' + game_id;
 
-                    let message = `To Automatically Switch to this game, create a browser source with the following settings.<br><br>
-                    <strong>URL</strong>: <input id="readOnlyField" type="text" readonly value="${browser_url}"> <button onclick="copyToClipboard()">Copy to clipboard</button><br><br>
+                    let message = `To Automatically Switch to this game, create a browser source with the following settings.<br><br><hr><br>
+                    <strong>Browser Source URL</strong><br><br>
+                    <input id="readOnlyField" type="text" readonly value="${browser_url}"><br><br>
+                    <button onclick="copyToClipboard()">Copy to clipboard</button><br><br>
                     Shutdown source when not visible = <strong>ENABLED</strong><br>
-                    Refresh browser when scene becomes active = <strong>ENABLED</strong>`;
+                    Refresh browser when scene becomes active = <strong>ENABLED</strong><br><br><hr><br>
+                    <strong>If you want to switch to a different game than listed above...</strong><br><br>
+                    Set your stream to the game you want to switch to, then reload this page.<br>
+                    You will then be given a new browser source url that will automatically switch to that game.`;
 
                     $('body').append('<div>' + message + '</div>');
 				}
@@ -72,6 +77,7 @@ class SwitchGames {
 		reply.then((response) => {
             if (response.ok) {
                 console.log('Game Updated!');
+                $('body').append('<div>Game Updated!</div>');
             } else {
                 // console.log('Game Update Failed!');
                 location.href = location.protocol + '//' + location.host + location.pathname
